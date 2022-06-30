11fc3c67916c34dc6f7d09e52663b90ec9e31838
Mariia <whitesheepa@gmail.com>
2022-06-30 22:34:31 +0300

    build|

---
+++ docs/precache-manifest.cab77a1856faafb30e146ee8820e88b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4686fecc671f74d95c333b74f88b9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c72a2426db8aa32e89e4",
+    "url": "./static/css/2.0c96c983.chunk.css"
+  },
+  {
+    "revision": "99350790b0c093a1d511",
+    "url": "./static/css/main.f57af821.chunk.css"
+  },
+  {
+    "revision": "c72a2426db8aa32e89e4",
+    "url": "./static/js/2.c0a1c63c.chunk.js"
+  },
+  {
+    "revision": "99350790b0c093a1d511",
+    "url": "./static/js/main.ce43ea52.chunk.js"
+  },
+  {
+    "revision": "f7e8ed073c95474e7958",
+    "url": "./static/js/runtime-main.34c10522.js"
+  }
+]);
